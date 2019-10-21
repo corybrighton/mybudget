5a0d9cecfac333c94b1967a21e94a7b79c5e80c8
Cory <corybrighton01@gmail.com>
2019-10-21 15:09:49 -0600

    Cors are working

--- Budgeteer/src/app/list/list.page.ts
+++ Budgeteer/src/app/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-list',
@@ -20,14 +21,25 @@ export class ListPage implements OnInit {
     'build'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
+  constructor(private data: WeatherService) {
+    for (let i = 1; i < 6; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+    data.getWeather().subscribe(res => {
+      res.forEach(item => this.items.push({
+        title: `The Weather for ${item['date']}`,
+        note: `Will be ${item['temperatureF']} degrees and ${item['summary']}`,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      }))
+      console.log(res)
+    }
+    );
+    console.log("Star Wars flims here");
+    data.getStarWars().subscribe(res => console.log(res));
   }
 
   ngOnInit() {
