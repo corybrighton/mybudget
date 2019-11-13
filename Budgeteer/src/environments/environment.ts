14f67d983bfce7f78f0d0fd75923083cd31dd84f
Cory <corybrighton01@gmail.com>
2019-11-13 10:22:43 -0700

    remove environment

--- Budgeteer/src/environments/environment.ts
+++ Budgeteer/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   client_id: "Rz8E85nfzVXk2e60022wSMsVWWacduMA",
-  domain: "dev-4q9ch1pp.auth0.com"
+  domain: "dev-4q9ch1pp.auth0.com",
+  site: "https://localhost:5001"
 };
 
 /*
